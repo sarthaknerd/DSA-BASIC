6d5ebdf3bcd27efcf749a60cc1eebf145bb32310
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2023-02-26 22:47:37 +0530

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespa std;
+
+int binarySearch(int arr[],int size ,int key){
+    int start = 0;
+    int end = int size-1;
+    int mid = start +(end - start)/2;
+    
+    while(start<=end){
+        if(arr[mid]==key){
+            return mid;
+        }
+        if(key>arr[mid]){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+         mid = start + (end -start)/2;
+    }
+    return -1;
+}
