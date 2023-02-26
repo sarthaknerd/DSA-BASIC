8121e428cbd0c50b35c13bc36e6b02de3e25b87c
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2023-02-27 00:09:43 +0530

    Create 1281.cpp

---
+++ 1281.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        //int ans = 0 ;
+        int sum =0 ;
+        int Product =1;
+        while(n!=0){
+            int digit = n%10;
+            sum +=digit;
+            Product *=digit;
+            n=n/10;
+        }
+         
+        return ( Product -sum);
+    }
+};
