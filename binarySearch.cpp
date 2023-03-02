95bfef980aed5ea8d8cd7b695b2ed0e61d175670
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2023-03-02 08:57:41 +0530

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
-//2
+//3
 int binarySearch(int arr[],int size ,int key){
-    int start = 0;
-    int end = int size-1;
-    int mid = start +(end - start)/2;
-    
+    int strat = 0;
+    int end = size-1;
+    int mid = start+(end-start)/2;
     while(start<=end){
-        if(arr[mid]==key){
+            if(arr[mid]==key){
             return mid;
         }
-        if(key>arr[mid]){
-            start = mid+1;
+        else if(key>mid){
+             start = mid + 1;    
         }
         else{
             end = mid-1;
         }
-         mid = start + (end -start)/2;
-    }
+        mid = start +(end-start)/2; 
+}
+    
     return -1;
 }
