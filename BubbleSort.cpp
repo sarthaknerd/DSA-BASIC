fc3946804609a93f1464be4dacfacaa3f3ef6720
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2023-03-03 09:13:48 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,13 +1,11 @@
  #include <iostream>
-
-int main(){
-  for(int i =1 ;i<n;i++){
-   
-    for(int j = 0 ;j<n-i;j++){
-      if(arr[j] > arr[j+1]){
-        swap(arr[j],arr[j+1]);
-      }
-  }
- 
+//2
+ int main(){
+  for(int i = 1;i<n;i++){
+   for(int j = 0; j<n-i;j++){
+    if(arr[j]>arr[j+1]){
+    swap(arr[j],arr[j+1]); 
+    }
+    }
   
-}
+ }
